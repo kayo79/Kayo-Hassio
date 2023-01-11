24e3986c5fe2066046dc4a70e353fa8cd9ccb730
kayo79 <64925376+kayo79@users.noreply.github.com>
2023-01-11 11:57:00 +0100

    Add files via upload

---
+++ mopidy-bushvin/const.py
@@ -0,0 +1,11 @@
+"""Constants for the Mopidy integration."""
+
+DOMAIN = "mopidy"
+ICON = "mdi:music-note"
+DEFAULT_NAME = "Mopidy"
+DEFAULT_PORT = 6680
+SERVICE_SNAPSHOT = "snapshot"
+SERVICE_RESTORE = "restore"
+SERVICE_SEARCH = "search"
+CACHE_TITLES = {}
+CACHE_ART = {}
